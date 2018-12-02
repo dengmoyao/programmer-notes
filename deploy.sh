f5a3fe4caa43943df269fbed3af5697f651c60c6
tomoyadeng <tomoyadeng@gmail.com>
2018-12-02 15:46:23 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
-npm install -g gitbook-cli@2.3.1
 cd gitbook
-
-gitbook install
-gitbook build
 
 if [ "$(docker ps -q -f name=programmer-notes)" ]; then
 	docker stop programmer-notes
 fi
+
+docker run --rm -v "$PWD:/gitbook" -p 4000:4000 tomoyadeng/gitbook:3.2.3 gitbook build
 
 docker container run \
   -d \
