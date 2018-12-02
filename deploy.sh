a02474ba4bbf7d3ea3483cd2347248e69fbf430e
tomoyadeng <tomoyadeng@gmail.com>
2018-12-02 15:52:29 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ if [ "$(docker ps -q -f name=programmer-notes)" ]; then
 	docker stop programmer-notes
 fi
 
+docker run --rm -v "$PWD:/gitbook" -p 4000:4000 tomoyadeng/gitbook:3.2.3 gitbook install
+
 docker run --rm -v "$PWD:/gitbook" -p 4000:4000 tomoyadeng/gitbook:3.2.3 gitbook build
 
 docker container run \
